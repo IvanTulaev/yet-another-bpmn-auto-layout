089bdba0cbe0efb89684bfc12e367f8b66546cc5
Ivan Tulaev <ivan.aks.msk@gmail.com>
2025-11-15 00:32:25 +0300

    # Еще один большой коммит с рефакторингом
    Восстановлено большинство функционала 63/15
    
    ## ref:
      - Добавлен граф как хранилище для ребер
      - Переписал разделение грида с использованием графа
      - Переписал move
      - Переписал hasAnyCross(edges)
      - Переписал isCrossed(position, byVertical)
      - Переписал move forward
      - Переписал шейки
      - Переписал поиск цепочек
      - Переписал получение исходящих и входящих элементов
      - Удалил лишний код
    
    fix:
      - debug mode работает с expanded и pools
    
     ## feat:
      - Уменьшено кол-во пересечений: при отрисовке boundary первыми отрисовываются те, у которых потомки ниже и правее
      - На диаграмму добавлены dataObject и dataStore на общих основаниях как будто это обычные элементы

--- test/LayoutSpec.js
+++ test/LayoutSpec.js
@@ -3,7 +3,7 @@ import fs from 'node:fs';
 import path from 'node:path';
 import url from 'node:url';
 
-import { layoutProcess } from 'bpmn-auto-layout-feat-ivan-tulaev';
+import { layoutProcess } from '../lib/index.js';
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
